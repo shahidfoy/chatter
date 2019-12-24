1a4eba49e8b67b2bc9d9c01fcb925e9dec87d352
Shahid Foy <shahidfoy@gmail.com>
2019-12-24 10:44:45 -0700

    styling users component

--- chatter/chat-app/src/app/streams/streams-routing.module.ts
+++ chatter/chat-app/src/app/streams/streams-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { StreamsComponent } from './streams.component';
 import { AuthGuard } from '../guards/auth.guard';
