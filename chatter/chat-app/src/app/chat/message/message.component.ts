b80de5571d6c638d66b350dbbc4db8c736d6653c
Shahid Foy <shahidfoy@gmail.com>
2019-12-30 17:09:31 -0700

    styling message component

--- chatter/chat-app/src/app/chat/message/message.component.ts
+++ chatter/chat-app/src/app/chat/message/message.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-message',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
-  constructor() { }
+  chatingWithUsername: string;
+
+  data = [
+    { name: 'Lily' },
+    { name: 'Lily' },
+    { name: 'Lily' },
+    { name: 'Lily' },
+    { name: 'Lily' },
+    { name: 'Lily' },
+    { name: 'Lily' },
+    { name: 'Lily' },
+    { name: 'Lily' },
+    { name: 'Lily' },
+    { name: 'Lily' },
+    { name: 'Lily' }
+  ];
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.chatingWithUsername = this.activatedRoute.snapshot.params.username;
   }
-
 }
