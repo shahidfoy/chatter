3f92934318739ca4cf083f24c60af3645fbe65c5
Shahid Foy <shahidfoy@gmail.com>
2020-01-27 17:44:54 -0700

    implementing profile image upload

--- chatter/chat-app/src/app/streams/profile/profile.component.ts
+++ chatter/chat-app/src/app/streams/profile/profile.component.ts
@@ -10,8 +10,9 @@ import { timeFromNow } from 'src/app/shared/shared.utils';
 import { HttpErrorResponse } from '@angular/common/http';
 import { PostService } from '../services/post.service';
 import * as _ from 'lodash';
-import { NzNotificationService } from 'ng-zorro-antd';
+import { NzNotificationService, NzMessageService, UploadFile } from 'ng-zorro-antd';
 import { UserFollowing } from '../interfaces/user-following.interface';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -20,6 +21,7 @@ import { UserFollowing } from '../interfaces/user-following.interface';
 })
 export class ProfileComponent implements OnInit {
 
+  isVisible = false;
   isMobile: boolean;
   payload: PayloadData;
   username: string;
@@ -28,6 +30,9 @@ export class ProfileComponent implements OnInit {
   isLoggedInUser: boolean;
   followingUser: boolean;
 
+  loading = false;
+  avatarUrl: string;
+
   constructor(
     private userService: UserService,
     private postService: PostService,
@@ -35,6 +40,7 @@ export class ProfileComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private applicationStateService: ApplicationStateService,
     private notification: NzNotificationService,
+    private msg: NzMessageService,
     private router: Router,
   ) { }
 
@@ -60,6 +66,89 @@ export class ProfileComponent implements OnInit {
     });
     this.userService.receiveNewFollowSocket().subscribe(() => {
       this.getUser();
+    });
+  }
+
+  // TODO:: CURRENTLY HERE
+
+  uploadProfileImage(username: string): void {
+    console.log('username', username);
+    this.isVisible = true;
+  }
+
+  handleModalOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleModalCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
+
+  beforeUpload = (file: File) => {
+    return new Observable((observer: Observer<boolean>) => {
+      const isJPG = file.type === 'image/jpeg';
+      if (!isJPG) {
+        this.msg.error('You can only upload JPG file!');
+        observer.complete();
+        return;
+      }
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      if (!isLt2M) {
+        this.msg.error('Image must smaller than 2MB!');
+        observer.complete();
+        return;
+      }
+      // check height
+      this.checkImageDimension(file).then(dimensionRes => {
+        if (!dimensionRes) {
+          this.msg.error('Image only 300x300 above');
+          observer.complete();
+          return;
+        }
+
+        observer.next(isJPG && isLt2M && dimensionRes);
+        observer.complete();
+      });
+    });
+  }
+
+  handleChange(info: { file: UploadFile }): void {
+    switch (info.file.status) {
+      case 'uploading':
+        this.loading = true;
+        break;
+      case 'done':
+        // Get this url from response in real world.
+        this.getBase64(info.file!.originFileObj!, (img: string) => {
+          this.loading = false;
+          this.avatarUrl = img;
+        });
+        break;
+      case 'error':
+        this.msg.error('Network error');
+        this.loading = false;
+        break;
+    }
+  }
+
+  private getBase64(img: File, callback: (img: string) => void): void {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result!.toString()));
+    reader.readAsDataURL(img);
+  }
+
+  private checkImageDimension(file: File): Promise<boolean> {
+    return new Promise(resolve => {
+      const img = new Image(); // create image
+      img.src = window.URL.createObjectURL(file);
+      img.onload = () => {
+        const width = img.naturalWidth;
+        const height = img.naturalHeight;
+        window.URL.revokeObjectURL(img.src!);
+        resolve(width === height && width >= 300);
+      };
     });
   }
 
