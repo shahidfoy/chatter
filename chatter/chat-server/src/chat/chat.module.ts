f4cabe2abd59cfd10295e13056c3243a64e17a84
Shahid Foy <shahidfoy@gmail.com>
2020-03-06 17:42:00 -0700

    updated log

--- chatter/chat-server/src/chat/chat.module.ts
+++ chatter/chat-server/src/chat/chat.module.ts
@@ -6,12 +6,10 @@ import { UserSchema } from 'src/users/models/user.model';
 import { ConversationSchema } from './models/conversation.model';
 import { MessageSchema } from './models/message.model';
 import { ChatGateway } from './gateways/chat.gateway';
-import { ChatListSchema } from './models/chat-list.model';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: 'ChatList', schema: ChatListSchema },
       { name: 'Conversation', schema: ConversationSchema },
       { name: 'Message', schema: MessageSchema },
       { name: 'User', schema: UserSchema },
