f4949c716d3d3128e6f8987b1fc7a1325c7a582e
initrail <oneofuser25@gmail.com>
2020-05-28 21:09:41 -0700

    message length added

--- chatter/chat-server/src/chat/chat.service.ts
+++ chatter/chat-server/src/chat/chat.service.ts
@@ -7,133 +7,126 @@ import { Conversation } from './models/conversation.model';
 
 @Injectable()
 export class ChatService {
+  private readonly MESSAGE_LENGTH = 10;
+  constructor(
+    @InjectModel('Message') private readonly messageModel: Model<Message>,
+    @InjectModel('Conversation')
+    private readonly conversationModel: Model<Conversation>,
+  ) {}
 
-    constructor(
-        @InjectModel('Message') private readonly messageModel: Model<Message>,
-        @InjectModel('Conversation') private readonly conversationModel: Model<Conversation>,
-    ) {}
+  /**
+   * gets conversations list for selected user id
+   * @param userId user id
+   */
+  async getConversationsList(userId: string): Promise<Conversation[]> {
+    const conversationsList = await this.conversationModel
+      .find({
+        $or: [{ senderId: userId }, { receiverId: userId }],
+      })
+      .populate('messageId')
+      .populate('senderId')
+      .populate('receiverId')
+      .sort({ createdAt: -1 });
+    return conversationsList;
+  }
 
-    /**
-     * gets conversations list for selected user id
-     * @param userId user id
-     */
-    async getConversationsList(userId: string): Promise<Conversation[]> {
-        const conversationsList = await this.conversationModel.find({
-            $or: [
-                { senderId: userId },
-                { receiverId: userId },
-            ],
-        })
-        .populate('messageId')
-        .populate('senderId')
-        .populate('receiverId')
-        .sort({ createdAt: -1 });
-        return conversationsList;
-    }
+  /**
+   * gets messages between two users
+   * @param senderId senders id
+   * @param receiverId receivers id
+   */
+  async getMessages(
+    senderId: string,
+    receiverId: string,
+  ): Promise<Conversation> {
+    const conversations = await this.conversationModel
+      .findOne({
+        $or: [
+          {
+            $and: [{ senderId }, { receiverId }],
+          },
+          {
+            $and: [{ senderId: receiverId }, { receiverId: senderId }],
+          },
+        ],
+      })
+      .populate('messageId')
+      .populate('senderId')
+      .populate('receiverId');
+    return conversations;
+  }
 
-    /**
-     * gets messages between two users
-     * @param senderId senders id
-     * @param receiverId receivers id
-     */
-    async getMessages(senderId: string, receiverId: string): Promise<Conversation> {
-        const conversations = await this.conversationModel.findOne({
-            $or: [
-                {
-                    $and: [
-                        { senderId },
-                        { receiverId },
-                    ],
-                },
-                {
-                    $and: [
-                        { senderId: receiverId },
-                        { receiverId: senderId },
-                    ],
-                },
-            ],
-        })
-        .populate('messageId')
-        .populate('senderId')
-        .populate('receiverId');
-        return conversations;
-    }
+  /**
+   * marks receivers messages that are sent to the logged in user as read
+   * @param sender logged in user
+   * @param receiver receiver of logged in users messages
+   */
+  async markReceiverMessages(sender: string, receiver: string) {
+    const messages = await this.messageModel.aggregate([
+      { $unwind: '$message' },
+      {
+        $match: {
+          $and: [
+            { 'message.sendername': receiver, 'message.receivername': sender },
+          ],
+        },
+      },
+    ]);
 
-    /**
-     * marks receivers messages that are sent to the logged in user as read
-     * @param sender logged in user
-     * @param receiver receiver of logged in users messages
-     */
-    async markReceiverMessages(sender: string, receiver: string) {
-        const messages = await this.messageModel.aggregate([
-            { $unwind: '$message' },
+    if (messages.length > 0) {
+      try {
+        messages.forEach(async (messageContents: any) => {
+          await this.messageModel.updateOne(
             {
-                $match: {
-                    $and: [{ 'message.sendername': receiver, 'message.receivername': sender }],
-                },
+              'message._id': messageContents.message._id,
             },
-        ]);
-
-        if (messages.length > 0) {
-            try {
-                messages.forEach(async (messageContents: any) => {
-                    await this.messageModel.updateOne({
-                        'message._id': messageContents.message._id,
-                    }, {
-                        $set: {'message.$.isRead': true },
-                    });
-                });
-            } catch (err) {
-                throw new InternalServerErrorException('Error: marking receiver messages');
-            }
-        }
-    }
-
-    /**
-     * check if chat conversation exists gets conversation or creates a new one
-     * creates and sends a message and updates both users chatList
-     * @param user logged in user
-     * @param senderId sender id
-     * @param receiverId receiver id
-     * @param receiverName receiver name
-     * @param message chat message
-     */
-    async sendMessage(user: User, senderId: string, receiverId: string, receiverName: string, message: string): Promise<Message> {
-        const conversations = await this.conversationModel.find({
-            $or: [
-                { senderId, receiverId },
-                { senderId: receiverId, receiverId: senderId },
-            ],
+            {
+              $set: { 'message.$.isRead': true },
+            },
+          );
         });
+      } catch (err) {
+        throw new InternalServerErrorException(
+          'Error: marking receiver messages',
+        );
+      }
+    }
+  }
 
-        if (conversations.length > 0) {
-            return await this.messageModel.updateOne({
-                _id: conversations[0].messageId,
-            }, {
-                $push: {
-                    message: {
-                        senderId,
-                        receiverId,
-                        sendername: user.username,
-                        receivername: receiverName,
-                        body: message,
-                        isRead: false,
-                        createdAt: new Date(),
-                    },
-                },
-            }).then(async () => {
-                await this.conversationModel.updateOne({ _id: conversations[0]._id }, { createdAt: new Date() });
-                return await this.messageModel.findOne({ _id: conversations[0].messageId });
-            }).catch(error => {
-                throw new InternalServerErrorException({ message: `Error message occured ${error}`});
-            });
-        } else {
-            const messageBody: Partial<Message> = {
-                sender: user.username,
-                receiver: receiverName,
-                message: [],
-            };
-            const messageContents: MessageContents = {
+  /**
+   * check if chat conversation exists gets conversation or creates a new one
+   * creates and sends a message and updates both users chatList
+   * @param user logged in user
+   * @param senderId sender id
+   * @param receiverId receiver id
+   * @param receiverName receiver name
+   * @param message chat message
+   */
+  async sendMessage(
+    user: User,
+    senderId: string,
+    receiverId: string,
+    receiverName: string,
+    message: string,
+  ): Promise<Message> {
+    if (message.length > this.MESSAGE_LENGTH)
+      message = message.substring(0, this.MESSAGE_LENGTH);
+    const conversations = await this.conversationModel.find({
+      $or: [
+        { senderId, receiverId },
+        { senderId: receiverId, receiverId: senderId },
+      ],
+    });
+
+    if (conversations.length > 0) {
+      return await this.messageModel
+        .updateOne(
+          {
+            _id: conversations[0].messageId,
+          },
+          {
+            $push: {
+              message: {
                 senderId,
                 receiverId,
                 sendername: user.username,
@@ -141,21 +134,57 @@ export class ChatService {
                 body: message,
                 isRead: false,
                 createdAt: new Date(),
-            };
-            messageBody.message.push(messageContents);
-            return await this.messageModel.create(messageBody)
-                            .then(async (messageRes: Message) => {
-                                const body: Partial<Conversation> = {
-                                    senderId,
-                                    receiverId,
-                                    messageId: messageRes._id,
-                                    createdAt: new Date(),
-                                };
-                                await this.conversationModel.create(body);
-                                return messageRes;
-                            }).catch(error => {
-                                throw new InternalServerErrorException({ message: `Error message occured ${error}`});
-                            });
-        }
+              },
+            },
+          },
+        )
+        .then(async () => {
+          await this.conversationModel.updateOne(
+            { _id: conversations[0]._id },
+            { createdAt: new Date() },
+          );
+          return await this.messageModel.findOne({
+            _id: conversations[0].messageId,
+          });
+        })
+        .catch(error => {
+          throw new InternalServerErrorException({
+            message: `Error message occured ${error}`,
+          });
+        });
+    } else {
+      const messageBody: Partial<Message> = {
+        sender: user.username,
+        receiver: receiverName,
+        message: [],
+      };
+      const messageContents: MessageContents = {
+        senderId,
+        receiverId,
+        sendername: user.username,
+        receivername: receiverName,
+        body: message,
+        isRead: false,
+        createdAt: new Date(),
+      };
+      messageBody.message.push(messageContents);
+      return await this.messageModel
+        .create(messageBody)
+        .then(async (messageRes: Message) => {
+          const body: Partial<Conversation> = {
+            senderId,
+            receiverId,
+            messageId: messageRes._id,
+            createdAt: new Date(),
+          };
+          await this.conversationModel.create(body);
+          return messageRes;
+        })
+        .catch(error => {
+          throw new InternalServerErrorException({
+            message: `Error message occured ${error}`,
+          });
+        });
     }
+  }
 }
